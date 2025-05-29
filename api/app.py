57ad98efa62e35720e0dbc4380101d0270ea1a7d
Ismail Sherif <ismailsherifwork@gmail.com>
2025-05-29 03:58:59 +0300

    new endpoint: help

--- api/app.py
+++ api/app.py
@@ -9,3 +9,7 @@ async def root():
 @app.get("/about")
 async def about():
     return {"message": "About Page: This is the about page"}
+
+@app.get("/help")
+async def about():
+    return {"message": "Help Page: This is the help page"}
